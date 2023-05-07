45365e9abb4652d68da7df96c98265609e0b626d
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-05-07 19:45:44 +0800

    Removed crossword generation script in manager file

--- Assets/CrossWordGridManager.cs
+++ Assets/CrossWordGridManager.cs
@@ -1,55 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CrossWordGridManager : MonoBehaviour
 {
-    public static CrossWordGridManager instance;
+  public static CrossWordGridManager instance;
 
-    [Header("Grid Component")]
-    [SerializeField] private int gridSize;
-    [SerializeField] private float gridLength;
-    private float cellSize;
-    Grid<CrossWordObject> grid;
+  [Header("Grid Component")]
+  [SerializeField] private int gridSize;
+  [SerializeField] private float gridLength;
+  private float cellSize;
+  Grid<CrossWordObject> grid;
 
-    private void Awake()
+  private void Awake()
+  {
+      if(instance == null) instance = this;
+  }
+
+  public void CreateCrossWordGrid(List<string> words, int gridSize, float cellSize)
+  {
+    this.gridSize = gridSize;
+    
+      GenerateGrid(gridSize, gridSize, 0.8f, transform.position);
+  }
+  
+  public void GenerateGrid(int width, int height, float cellSize, Vector3 position)
+  {
+      grid = new Grid<CrossWordObject>(width, height, cellSize, position, (Grid<CrossWordObject> g, int x, int y) => new CrossWordObject(g, x, y));
+  }
+   
+    private Tuple<int,int,int,int> GetCornerSize(Dictionary<Tuple<int, int>, char> gridDictionary)
+  {
+    int min_x = 0, max_x = 0;
+    int min_y = 0, max_y = 0;
+
+    foreach(var (x,y) in gridDictionary.Keys)
     {
-        if(instance == null) instance = this;
+      min_x = Mathf.Min(min_x, x);
+      max_x = Mathf.Max(max_x, x);
+      min_y = Mathf.Min(min_y, y);
+      max_y = Mathf.Max(max_y, y);
     }
 
-    public void CreateCrossWordGrid(List<string> words, int gridSize, float cellSize)
-    {
-      this.gridSize = gridSize;
-      
-        GenerateGrid(gridSize, gridSize, 0.8f, transform.position);
-        
-        //PLACE FIRST LETTER INSIDE THE GRID IN THE MIDDLE
-        PlaceFirstLetter(words[0]);
-    }
-    
-    public void GenerateGrid(int width, int height, float cellSize, Vector3 position)
-    {
-        grid = new Grid<CrossWordObject>(width, height, cellSize, position, (Grid<CrossWordObject> g, int x, int y) => new CrossWordObject(g, x, y));
-    }
-    
-    private void PlaceFirstLetter(string word)
-    {
-      //PLACE IT IN THE FUCKING MIDDLE
-      int startX = Mathf.RoundToInt(gridSize/2);
-      int startY = gridSize - 1;
-      
-      PlaceWordVertically();
-    }
-    
-    private void PlaceWordVertically(int startX, int startY, string word)
-    {
-      for(int i = 0; i < word.length; i++)
-      {
-        CrossWordObject node = grid.getGridObject(startX, startY+ i);
-        
-        //ASSIGN CORRESPONDING WORD AND LETTER
-        
-        
-      }
-    }
-    
+    return new Tuple<int, int, int, int>(min_x, min_y, max_x, max_y);
+  }
     
 }
