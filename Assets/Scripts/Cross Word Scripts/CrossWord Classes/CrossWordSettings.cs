9d87ebbe39b0741b157b156369cdd15ce53b0ffa
Joshua Marquez <Joshua.marquez0718@gmail.com>
2023-05-12 13:10:47 +0800

    removed randomizeQuestion

--- Assets/Scripts/Cross Word Scripts/CrossWord Classes/CrossWordSettings.cs
+++ Assets/Scripts/Cross Word Scripts/CrossWord Classes/CrossWordSettings.cs
@@ -7,7 +7,6 @@ public class CrossWordSettings
     public TriviaQuestion[] triviaQuestions;
     public int timeLimit;
     public bool enableHints;
-    public bool randomizeQuestion;
     public string basedGrading;
     public bool showCorrectAnswer;
     public int numOfLettersToShow;
